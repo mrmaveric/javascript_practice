250420544d0a6140719794c09dbd0be0c8eee8e5
ChloeGyuriKim <missbodre@gmail.com>
2014-12-09 16:22:46 +0900

    터치슬라이더/자동 롤링배너, 제어버튼을 활용한 알림판,

--- jQueryProjectPractice1/js/main.js
+++ jQueryProjectPractice1/js/main.js
@@ -32,8 +32,60 @@ $(function(){
     
     
   /*롤링 버튼 배너*/
-  
-    
+  $("#roll_banner_wrap dd").not(":first").hide();
+
+    var onBtn = $("#roll_banner_wrap dt a:first");
+
+  $("#roll_banner_wrap dt a").on("click", function () {
+      $("#roll_banner_wrap dd:visible").hide();
+
+      $("img",onBtn).attr("src",
+      $("img",onBtn).attr("src").replace("over.gif","out.git"));
+
+      var num=$("#roll_banner_wrap dt a").index(this);
+      $("#roll_banner_wrap dd").eq(num).show();
+      $("img",this).attr("src",
+      $("img",this).attr("src").replace("out.gif","over.gif"));
+
+      onBtn=$(this);
+
+      return false;
+  });
+
+  $(".playBtn").on("click",function(){
+      clearTimeout(auto1);
+      auto1=setTimeout(autoPlay,1000);
+
+      $("img",this).attr("src",$("img",this).attr("src").replace("off.gif","on.gif"));
+
+      $(".stopBtn img").attr("src",
+      $(".stopBtn img").attr("src").replace("on.gif","off.gif"));
+
+      return false;
+  });
+
+  $(".stopBtn").on("click",function(){
+      clearTimeout(auto1);
+      $("img",this).attr("src",
+      $("img",this).attr("src").replace("off.gif","on.gif"));
+
+      $(".playBrn img").attr("src",
+      $(".playBtn img").attr("src").replace("on.gif","off.gif"));
+
+      return false;
+  });
+
+  var btnNum =0;
+  function autoPlay(){
+      btnNum++;
+
+      if(btnNum>=4) btnNum=0;
+      $("#roll_banner_wrap dt a").eq(btnNum).trigger("click");
+
+      auto1=setTimeout(autoPlay,4000);
+  }
+
+  var auto1= setTimeout(autoPlay,3000);
   /*탭메뉴*/
   
   /* 베스트북 슬라이더 */
